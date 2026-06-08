0d500d9df1dd1cc0ec0a2c48b0aff146e68e4765
RajendraMallina <rajendramallina@gmail.com>
2026-06-08 09:58:04 +0530

    "added JWT RSA algorithm"

--- src/main/java/itm/codingmaxima/contact/application/service/JWTService.java
+++ src/main/java/itm/codingmaxima/contact/application/service/JWTService.java
@@ -6,12 +6,20 @@ import io.jsonwebtoken.security.Keys;
 import itm.codingmaxima.contact.application.model.AppRole;
 import itm.codingmaxima.contact.application.model.AppUserDetails;
 import itm.codingmaxima.contact.application.model.PhoneBookUser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
@@ -21,13 +29,56 @@ import java.util.function.Function;
 @Service
 public class JWTService {
 
-    private String secreatKey =  "";
+    @Value("${private.key.path}")
+    private String privateKeyFilePath;
 
-    public JWTService() throws NoSuchAlgorithmException {
+    @Value("${public.key.path}")
+    private String publicKeyFilePath;
 
-        KeyGenerator genKey = KeyGenerator.getInstance("HmacSHA256");
-        SecretKey seKey = genKey.generateKey();
-        secreatKey = Base64.getEncoder().encodeToString(seKey.getEncoded());
+
+    private PrivateKey loadPrivateKey(String privateKeyPath) {
+        try {
+            Resource resource = new ClassPathResource(privateKeyPath);
+
+            String key = new String(resource.getInputStream().readAllBytes());
+
+            key = key
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replace("-----END PRIVATE KEY-----", "")
+                    .replaceAll("\\s", "");
+
+            byte[] keyBytes = Base64.getDecoder().decode(key);
+
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
+            return keyFactory.generatePrivate(keySpec);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to load RSA private key", e);
+        }
+    }
+
+    public static PublicKey loadPublicKey(String publicKeyPath) {
+
+        try{
+            Resource resource = new ClassPathResource(publicKeyPath);
+            String key = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+            key = key
+                    .replace("-----BEGIN PUBLIC KEY-----", "")
+                    .replace("-----END PUBLIC KEY-----", "")
+                    .replaceAll("\\s", "");
+
+            byte[] keyBytes = Base64.getDecoder().decode(key);
+
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return keyFactory.generatePublic(keySpec);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public String generateToken(PhoneBookUser user){
@@ -44,14 +95,10 @@ public class JWTService {
                 .subject(user.getName())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(getKey())
+                .signWith(loadPrivateKey(privateKeyFilePath))
                 .compact();
     }
 
-    private SecretKey getKey() {
-        byte[] bytesKey = Base64.getDecoder().decode(secreatKey);
-        return Keys.hmacShaKeyFor(bytesKey);
-    }
 
     public String extractUserName(String jwtToken) {
        return extractClaims(jwtToken, Claims::getSubject);
@@ -60,7 +107,7 @@ public class JWTService {
     public <T> T extractClaims(String token, Function<Claims, T> typeOfClaim){
 
         Claims claims = Jwts.parser()
-                .verifyWith(getKey())
+                .verifyWith(loadPublicKey(publicKeyFilePath))
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
